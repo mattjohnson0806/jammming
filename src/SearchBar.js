d17305a1e0dcb3dcec1af62592155b98f10379a1
Leonydus Johnson <leonydus@MacBook-Pro-2.local>
2024-07-05 00:29:37 -0400

    Basic setup for search bar

---
+++ src/SearchBar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const SearchBar = () => {
+    return (
+        <div>
+            <input type='text' id='searchBar' />
+            <br /><br /> 
+            <button>SEARCH</button>
+        </div>
+    )
+}
+
+export default SearchBar;
